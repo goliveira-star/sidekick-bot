ad4b86d128021f8dbda00282c6520165cf7983d7
goliveira-star <g.oliveira@easygo.io>
2026-06-22 09:35:28 +1000

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def parse_subject(message: discord.Message) -> str:
     subjects = [
         "violence", "terrorism", "nudity", "pornography", "hate speech",
         "self-harm", "doxxing", "fraud", "game hacking", "harassment",
-        "protection of minors", "lawfulness", "child_endangerment", "bullying", "hate",
+        "protection of minors", "lawfulness", "rights", "child_endangerment", "bullying", "false_sensationalism", "hate",
     ]
     for s in subjects:
         if s in lower:
